bbc19b2ecbae662d0ccefc3f89d4930c2c00344e
Lussiez Florian <florianlussiez@gmail.com>
2019-09-22 17:51:15 +0200

    [FEAT] initial swagger

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express'),
 os = require('os');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
 const routerUser = require('./routerUser');
 const routerEtape = require('./routerEtape.js')
 const routerParcours = require('./routerParcours.js')
@@ -13,6 +16,7 @@ app.use(bodyParser.json());
 app.use('/api/v1', routerUser);
 app.use('/api/v1', routerEtape);
 app.use('/api/v1', routerParcours);
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const port = (process.env.PORT || '3000');
 
